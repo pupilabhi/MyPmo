cee58221022978671816a06754a744bdb6c924b1
Abhishek <abhishek@pupilland.com>
2019-02-05 18:21:47 +0530

    changed to 2.1.1 to 2.0.0 and corrected password

--- src/main/java/com/connectivity/vikray/controller/LoginController.java
+++ src/main/java/com/connectivity/vikray/controller/LoginController.java
@@ -1,10 +1,8 @@
-/*package com.connectivity.vikray.controller;
+package com.connectivity.vikray.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.connectivity.vikray.service.Projectservice;
 
 @RestController
 @RequestMapping(value="/login")
@@ -12,4 +10,3 @@ public class LoginController {
 	
 	
 }
-*/
