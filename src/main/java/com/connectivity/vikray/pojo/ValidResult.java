46c8afc56a5428d418cf380b4aa96009d56522e6
Pawan Kumar <pawan@connectivitysolutions.in>
2019-02-13 14:31:48 +0530

    valis Result

--- src/main/java/com/connectivity/vikray/pojo/ValidResult.java
+++ src/main/java/com/connectivity/vikray/pojo/ValidResult.java
@@ -10,13 +10,44 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
 @Component
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
-public class ValidResult implements Serializable{
+public class ValidResult implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	public Long ErrorCode;
-	public String ErrorMsg;
-	public Object data;
-	public UserDetails user;
-	
+
+	private Long ErrorCode;
+	private String ErrorMsg;
+	private Object data;
+	private UserDetails user;
+
+	public Long getErrorCode() {
+		return ErrorCode;
+	}
+
+	public void setErrorCode(Long errorCode) {
+		ErrorCode = errorCode;
+	}
+
+	public String getErrorMsg() {
+		return ErrorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		ErrorMsg = errorMsg;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public UserDetails getUser() {
+		return user;
+	}
+
+	public void setUser(UserDetails user) {
+		this.user = user;
+	}
 }
