ec2fb1f8d616755422cef1bcfb267cb94a392f7b
Pawan Kumar <pawan@connectivitysolutions.in>
2019-02-13 17:41:01 +0530

    constant class changed to interface

--- src/main/java/com/connectivity/vikray/constant/VikrayPmoConstant.java
+++ src/main/java/com/connectivity/vikray/constant/VikrayPmoConstant.java
@@ -1,5 +1,15 @@
 package com.connectivity.vikray.constant;
 
-public class VikrayPmoConstant {
+import java.util.Collections;
+import java.util.List;
 
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.calendar.CalendarScopes;
+
+public interface VikrayPmoConstant {
+
+	public static final String APPLICATION_NAME = "Google Calendar vikraypmo";
+	public static final String TOKENS_DIRECTORY_PATH = "tokens";
+	public static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 }
