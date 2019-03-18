5fde726eabe9a5771d2959078a615397e3f31c9d
Abhishek <abhishek@pupilland.com>
2019-03-18 12:01:38 +0530

    added SSL and redirecting port https 9999

--- src/main/java/com/connectivity/vikray/JpaApplication.java
+++ src/main/java/com/connectivity/vikray/JpaApplication.java
@@ -4,10 +4,18 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -20,7 +28,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		JpaApplication.class,
 		Jsr310JpaConverters.class 
 })
-public class JpaApplication {
+//@Configuration
+public class JpaApplication extends SpringBootServletInitializer{
 
 
 		@PostConstruct
@@ -31,6 +40,33 @@ public class JpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
 	}
+	
+	@Bean
+	public TomcatServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+		return tomcat;
+	}
+
+	private Connector getHttpConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(true);
+		connector.setRedirectPort(9999);
+		return connector;
+	}
+
 
 }
 
