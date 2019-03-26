e1675755fdce55a0ef717e65a85db492c9efe6b1
Abhishek <abhishek@pupilland.com>
2019-03-26 14:34:40 +0530

    update project made put

--- src/main/java/com/connectivity/vikray/controller/ProjectController.java
+++ src/main/java/com/connectivity/vikray/controller/ProjectController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,11 @@ import com.connectivity.vikray.service.ProjectService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController
 @RequestMapping(value= "/pmo/project", produces = "application/hal+json")
-@Api(value = "Project Services")
+@Api(value = "Project Services", description = "")
 public class ProjectController {
 
 	@Autowired
@@ -40,12 +42,13 @@ public class ProjectController {
 	
 	@ApiOperation(value = "Creates Project")
 	@PostMapping("/creatProject")
-	public ResponseEntity<ApiResponse> createProjects(@RequestBody Project project) {
+	public ResponseEntity<ApiResponse> createProjects(
+			@ApiParam(value = "Project object store in database table", required = true) @RequestBody Project project) {
 		return projectService.createProject(project);
 	}
 
 	@ApiOperation(value = "Update particular project")
-	@PostMapping("/updateProject")
+	@PutMapping("/updateProject")
 	public ResponseEntity<ApiResponse> updateProjects(@RequestBody Project project) {
 		return projectService.updateProject(project);
 	}
@@ -57,6 +60,7 @@ public class ProjectController {
 	}
 	
 	@GetMapping("/validate/{name}")
+	@ApiOperation(value = "Validate for duplicate project name")
 	public ResponseEntity<ApiResponse> checkDuplicate(@PathVariable String name){
 		return projectService.validateDuplicateProject(name);
 	}
